f7c22018f3df615b805e69c609d901496e0b9a8b
Закиров Раиль Наильевич <ZakirovRN@kaz.tn.corp>
2017-10-26 12:06:41 +0300

    first commit

---
+++ testBlockchain.py
@@ -0,0 +1,20 @@
+from genesis import create_genesis_block
+from genesis import next_block
+
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+num_of_blocks_to_add = 20
+
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+
+    blockchain.append(block_to_add)
+
+    previous_block = block_to_add
+
+    print "Block #{} has been added to the blockchain!".format(block_to_add.index)
+
+    print "Hash: {}".format(block_to_add.hash)
+
+    print "Data: {}\n".format(block_to_add.data)
